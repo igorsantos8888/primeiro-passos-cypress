32fd6297fba40ba5585fb3210fcbf622a38dbbdf
unknown <igorsantos888@gmail.com>
2025-04-11 11:52:32 -0300

    adding config about baseUrl in cypress.config.js

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,4 @@
-import userData from '../fixtures/user-data.json'
+import userData from '../fixtures/users/user-data.json'
 
 describe('Orange HRM Tests', () => {
 
@@ -12,7 +12,7 @@ describe('Orange HRM Tests', () => {
   }
 
   it('Login - success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
@@ -21,7 +21,7 @@ describe('Orange HRM Tests', () => {
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
